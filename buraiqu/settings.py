3996bb3f35bab732bfcdf22c0cf1bb1eeb8b3a85
Buraiqu <buraiquanwar@gmail.com>
2022-11-07 17:49:22 +0530

    'edit'

--- buraiqu/settings.py
+++ buraiqu/settings.py
@@ -52,8 +52,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware', 
-    'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+
+]
+
 ROOT_URLCONF = 'buraiqu.urls'
 
 TEMPLATES = [
@@ -126,7 +130,6 @@ STATICFILES_DIRS = [
 
     os.path.join(BASE_DIR, "static")
 ]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
